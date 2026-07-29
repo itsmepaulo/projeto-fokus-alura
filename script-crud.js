90e6f2e46b129c4841cbbe003b04ffb1cf513818
Paulo <iampaulo2002@gmail.com>
2026-07-29 09:19:30 -0300

    Update script-crud.js

--- script-crud.js
+++ script-crud.js
@@ -1,8 +1,18 @@
 // encontrar o botão adicionar tarefa.
 const adicionarTarefaBt = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textarea = document.querySelector('.app__form-textarea');
 
+const tarefas = [];
 
 adicionarTarefaBt.addEventListener('click', () =>{
     formAdicionarTarefa.classList.toggle('hidden'); //O toggle adiciona se não tiver e remove se tiver.
+})
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: textarea.value
+    }
+    tarefas.push(tarefa);
 })
