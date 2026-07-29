7e932d766d2e97554a4e0df78079ae070532a6d2
Paulo <iampaulo2002@gmail.com>
2026-07-29 16:12:00 -0300

    Update script-crud.js

--- script-crud.js
+++ script-crud.js
@@ -3,16 +3,17 @@ const adicionarTarefaBt = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textarea = document.querySelector('.app__form-textarea');
 
-const tarefas = [];
+const tarefas = []; //Lista de tarefas, por isso criado um array
 
 adicionarTarefaBt.addEventListener('click', () =>{
     formAdicionarTarefa.classList.toggle('hidden'); //O toggle adiciona se não tiver e remove se tiver.
 })
 
 formAdicionarTarefa.addEventListener('submit', (evento) => {
-    evento.preventDefault();
-    const tarefa = {
+    evento.preventDefault(); //Evita a página carregar ao clicar para enviar.
+    const tarefa = { //Objeto tarefa, ele vai capturar do textarea
         descricao: textarea.value
     }
-    tarefas.push(tarefa);
+    tarefas.push(tarefa); // Adiciona o objeto tarefa para o array tarefas.
+    localStorage.setItem('tarefas', tarefas);
 })
