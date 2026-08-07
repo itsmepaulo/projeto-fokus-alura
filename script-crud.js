fa229ecb346f3c62b32f2cac39c56d77a4829d2d
Paulo <iampaulo2002@gmail.com>
2026-08-07 14:14:22 -0300

    Update script-crud.js

--- script-crud.js
+++ script-crud.js
@@ -5,6 +5,35 @@ const textarea = document.querySelector('.app__form-textarea');
 
 const tarefas = []; //Lista de tarefas, por isso criado um array
 
+function criarElementoTarefa(tarefa) {
+    const li = document.createElement('li');
+    li.classList.add('app__section-task-list-item');
+
+    const svg = document.createElement('svg');
+    svg.innerHTML = ` 
+    <svg>
+        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
+        </svg>
+    </svg>
+    `
+    const paragrafo = document.createElement('p');
+    paragrafo.textContent = tarefa.descricao;
+    // paragrafo.classList.add('app__section-task-list-item-description');
+
+    const botao = document.createElement('button');
+    botao.classList.add('app_button-edit');
+    
+    const imagemBotao = document.createElement('img');
+    imagemBotao.setAttribute('src', 'imagens/edit.png');
+
+    botao.append(imagemBotao);
+
+    li.append(svg, paragrafo, botao);
+    
+}
+
 adicionarTarefaBt.addEventListener('click', () =>{
     formAdicionarTarefa.classList.toggle('hidden'); //O toggle adiciona se não tiver e remove se tiver.
 })
