47fd19e06ac80c04da9be53971c060bcedca9029
Paulo <iampaulo2002@gmail.com>
2026-08-13 14:38:16 -0300

    Update script-crud.js

--- script-crud.js
+++ script-crud.js
@@ -2,8 +2,12 @@
 const adicionarTarefaBt = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textarea = document.querySelector('.app__form-textarea');
+const ulTarefas = document.querySelector('.app__section-task-list');
 
-const tarefas = []; //Lista de tarefas, por isso criado um array
+
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []; //Lista de tarefas, por isso criado um array
+
+
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li');
@@ -11,30 +15,29 @@ function criarElementoTarefa(tarefa) {
 
     const svg = document.createElement('svg');
     svg.innerHTML = ` 
-    <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
     `
     const paragrafo = document.createElement('p');
     paragrafo.textContent = tarefa.descricao;
+    paragrafo.classList.add('app__section-task-list-item-description');
     // paragrafo.classList.add('app__section-task-list-item-description');
 
     const botao = document.createElement('button');
     botao.classList.add('app_button-edit');
-    
+
     const imagemBotao = document.createElement('img');
     imagemBotao.setAttribute('src', 'imagens/edit.png');
-
     botao.append(imagemBotao);
 
     li.append(svg, paragrafo, botao);
-    
+
+    return li
 }
 
-adicionarTarefaBt.addEventListener('click', () =>{
+adicionarTarefaBt.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden'); //O toggle adiciona se não tiver e remove se tiver.
 })
 
@@ -44,5 +47,14 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
         descricao: textarea.value
     }
     tarefas.push(tarefa); // Adiciona o objeto tarefa para o array tarefas.
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
     localStorage.setItem('tarefas', JSON.stringify(tarefas));  // Convertendo o array para uma string em formato JSON para poder armazenar.
+    textarea.value = '';
+    formAdicionarTarefa.classList.add('hidden');
+})
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
 })
